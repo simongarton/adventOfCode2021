9e888b668a061e349519902ad43f8f8de7a81e34
Simon Garton <simon.garton@gmail.com>
2021-12-02 17:50:47 +1300

    Tweaks for Vector.

--- src/main/java/com/simongarton/advent/Main.java
+++ src/main/java/com/simongarton/advent/Main.java
@@ -2,15 +2,24 @@ package com.simongarton.advent;
 
 import com.simongarton.advent.challenge.Challenge1;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
 
-    public static void main(final String[] args) {
+    public static void main(final String[] args) throws IOException {
 
         final Main main = new Main();
         main.run();
     }
 
-    private void run() {
-        new Challenge1().run();
+    private void run() throws IOException {
+        final String[] lines = Files.readAllLines(Paths.get("data/day1.txt")).toArray(new String[0]);
+        new Challenge1().run(lines);
     }
 }
--- src/main/java/com/simongarton/advent/challenge/Challenge1.java
+++ src/main/java/com/simongarton/advent/challenge/Challenge1.java
@@ -3,32 +3,27 @@ package com.simongarton.advent.challenge;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Challenge1 {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
 
-    private static final String FILENAME = "data/day1.txt";
     private static final String TITLE_1 = "Sonar Sweep 1";
     private static final String TITLE_2 = "Sonar Sweep 2";
 
-    public void run() {
-        this.part1();
-        this.part2();
+    public void run(final String[] lines) {
+        final List<Long> values = Arrays.stream(lines).mapToLong(Long::parseLong).boxed().collect(Collectors.toList());
+        this.part1(values);
+        this.part2(values);
     }
 
-    private long part1() {
-        final List<Long> longList = this.readValues();
+    private long part1(final List<Long> values) {
         final long start = System.currentTimeMillis();
-        final int increases = this.countIncreases(longList);
+        final int increases = this.countIncreases(values);
         this.logger.info(String.format("%s answer %d complete in %d ms",
                 TITLE_1,
                 increases,
@@ -41,22 +36,20 @@ public class Challenge1 {
         int index = 1;
         int increases = 0;
         while (index < values.size()) {
-            final long current = values.get(index);
+            final long current = values.get(index++);
             if (current > last) {
                 increases++;
             }
             last = current;
-            index++;
         }
         return increases;
     }
 
-    private long part2() {
-        final List<Long> longList = this.readValues();
+    private long part2(final List<Long> values) {
         final long start = System.currentTimeMillis();
         final List<Long> sumList = new ArrayList<>();
-        for (int i = 2; i < longList.size(); i++) {
-            sumList.add(longList.get(i - 2) + longList.get(i - 1) + longList.get(i));
+        for (int i = 2; i < values.size(); i++) {
+            sumList.add(values.get(i - 2) + values.get(i - 1) + values.get(i));
         }
         final int increases = this.countIncreases(sumList);
         this.logger.info(String.format("%s answer %d complete in %d ms",
@@ -64,14 +57,5 @@ public class Challenge1 {
                 increases,
                 System.currentTimeMillis() - start));
         return increases;
-    }
-
-    private List<Long> readValues() {
-        try (final Stream<String> lines = Files.lines(Paths.get(FILENAME))) {
-            return lines.mapToLong(Long::valueOf).boxed().collect(Collectors.toList());
-        } catch (final IOException e) {
-            this.logger.error(e.getMessage());
-            return Collections.emptyList();
-        }
     }
 }
