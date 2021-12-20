cbe80271454a6ca61bb8ab1ab6f2f0cd38015bf7
Simon Garton <simon.garton@gmail.com>
2021-12-21 01:14:01 +1300

    Reworked Packet for operator 1.

--- src/main/java/com/simongarton/advent/Main.java
+++ src/main/java/com/simongarton/advent/Main.java
@@ -105,7 +105,7 @@ public class Main {
 
     private void day16() throws IOException {
         final String[] lines = Files.readAllLines(Paths.get("data/day16-sample.txt")).toArray(new String[0]);
-        new Challenge16().run(lines);
+        new Challenge16().debug(lines);
     }
 
     private void day17() throws IOException {
--- src/main/java/com/simongarton/advent/challenge/Challenge16.java
+++ src/main/java/com/simongarton/advent/challenge/Challenge16.java
@@ -4,8 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Challenge16 {
@@ -15,17 +14,56 @@ public class Challenge16 {
     private static final String TITLE_1 = "Packet Decoder 1";
     private static final String TITLE_2 = "Packet Decoder 2";
     private static final boolean DEBUG = true;
+    private final Map<String, String> binaryTable = new HashMap();
 
     public void run(final String[] lines) {
+        this.buildBinaryTable();
         this.part1(lines);
         this.part2(lines);
+    }
+
+    public void debug(final String[] lines) {
+        this.buildBinaryTable();
+        for (final String hex : lines) {
+            final String binary = readPaddedBinaryFromHex(hex);
+            final Packet packet = new Packet(binary, 0);
+        }
+    }
+
+    private String readPaddedBinaryFromHex(final String hex) {
+        StringBuilder paddedBinary = new StringBuilder();
+        for (int i = 0; i < hex.length(); i++) {
+            final String c = hex.charAt(i) + "";
+            paddedBinary.append(this.binaryTable.get(c));
+        }
+        return paddedBinary.toString();
+    }
+
+    private void buildBinaryTable() {
+        this.binaryTable.clear();
+        for (int i = 0; i < 16; i++) {
+            final String hex = Integer.toString(i, 16);
+            final String binary = this.padTo("" + new BigInteger(hex, 16).toString(2), 4, "0");
+            this.binaryTable.put(hex.toUpperCase(Locale.ROOT), binary);
+        }
+    }
+
+    private String padTo(final String s, final int size) {
+        return this.padTo(s, size, " ");
+    }
+
+    private String padTo(final String s, final int size, final String replacement) {
+        if (s.length() > size) {
+            return new String(new char[size]).replace("\0", "*");
+        }
+        return new String(new char[size - s.length()]).replace("\0", replacement) + s;
     }
 
     protected long part1(final String[] lines) {
         final long start = System.currentTimeMillis();
         final long result = 0;
         final String hex = lines[0];
-        final String binary = new BigInteger(hex, 16).toString(2);
+        final String binary = readPaddedBinaryFromHex(hex);
         final Packet packet = new Packet(binary, 0);
         this.logger.info(String.format("%s answer %d complete in %d ms",
                 TITLE_1,
@@ -56,8 +94,10 @@ public class Challenge16 {
         private final Header header;
         private final PacketType packetType;
         private Integer literalValue;
+        private List<Packet> subPackets;
 
         public Packet(final String originalBinary, final int index) {
+            this.log("", "");
             this.originalBinary = originalBinary;
             this.log("originalBinary ", originalBinary);
             this.header = new Header(originalBinary.substring(0, 6));
@@ -65,40 +105,81 @@ public class Challenge16 {
             this.packetType = this.figurePacketType(this.header);
             this.log("header ", this.header.version + ":" + this.header.typeId + " = " + this.packetType);
             this.bitsRead += this.buildPacket(this.originalBinary.substring(6));
+            this.log("Packet", this.toString());
+        }
+
+        @Override
+        public String toString() {
+            if (this.packetType == PacketType.LITERAL_VALUE) {
+                return "Packet[" + this.packetType + " " + this.literalValue + "]";
+            } else {
+                return "Packet[" + this.packetType + " " + this.subPackets.size() + "]";
+            }
         }
 
         private void log(final String caption, final String value) {
             if (DEBUG) {
-                System.out.println(this.padTo(caption, 20) + " : " + value);
+                System.out.println(Challenge16.this.padTo(caption, 20) + " : " + value);
             }
-        }
-
-        private String padTo(final String s, final int size) {
-            if (s.length() > size) {
-                return new String(new char[size]).replace("\0", "*");
-            }
-            return new String(new char[size - s.length()]).replace("\0", " ") + s;
         }
 
         private int buildPacket(final String binary) {
             switch (this.packetType) {
                 case LITERAL_VALUE:
                     return this.buildPacketFromLiteralValue(binary);
+                case OPERATOR:
                 default:
                     return this.buildPacketFromOperator(binary);
             }
         }
 
         private int buildPacketFromOperator(final String binary) {
-            return 0;
+            int bitsReadForOperator = 0;
+            final int lengthTypeId = Integer.parseInt(binary.substring(0, 1));
+            bitsReadForOperator += 1;
+            switch (lengthTypeId) {
+                case 0:
+                    return bitsReadForOperator + this.readOperatorPacketsFromLength(binary.substring(1));
+                case 1:
+                default:
+                    return bitsReadForOperator + this.readOperatorPacketsFromCount(binary.substring(1));
+            }
+        }
+
+        private int readOperatorPacketsFromCount(final String substring) {
+            int bitsReadForOperatorLength = 0;
+            final int subPacketsCount = Integer.parseInt(substring.substring(0, 11), 2);
+            bitsReadForOperatorLength += 15;
+            final List<Packet> subPackets = new ArrayList<>();
+            int subPacketsBitsRead = 0;
+            while (subPackets.size() < subPacketsCount) {
+                final Packet packet = new Packet(substring.substring(15 + subPacketsBitsRead), 0);
+                subPackets.add(packet);
+                subPacketsBitsRead += packet.bitsRead;
+            }
+            return bitsReadForOperatorLength + subPacketsBitsRead;
+        }
+
+        private int readOperatorPacketsFromLength(final String substring) {
+            int bitsReadForOperatorLength = 0;
+            final int subPacketsBitLength = Integer.parseInt(substring.substring(0, 15), 2);
+            bitsReadForOperatorLength += 15;
+            int subPacketsBitsRead = 0;
+            this.subPackets = new ArrayList<>();
+            while (subPacketsBitsRead < subPacketsBitLength) {
+                final Packet packet = new Packet(substring.substring(15 + subPacketsBitsRead), 0);
+                this.subPackets.add(packet);
+                subPacketsBitsRead += packet.bitsRead;
+            }
+            return bitsReadForOperatorLength + subPacketsBitsRead;
         }
 
         private int buildPacketFromLiteralValue(final String binary) {
-            int index = 0;
+            int bitsReadForLiteralValue = 0;
             final List<FiveBitInteger> numbers = new ArrayList<>();
             while (true) {
-                final String numberString = binary.substring(index, index + 5);
-                index += 5;
+                final String numberString = binary.substring(bitsReadForLiteralValue, bitsReadForLiteralValue + 5);
+                bitsReadForLiteralValue += 5;
                 final FiveBitInteger number = new FiveBitInteger(numberString);
                 numbers.add(number);
                 if (number.lastBit) {
@@ -109,7 +190,7 @@ public class Challenge16 {
             this.log("hexString ", hexString);
             this.literalValue = Integer.parseInt(hexString, 2);
             this.log("literalValue ", this.literalValue + "");
-            return index;
+            return bitsReadForLiteralValue;
         }
 
         private PacketType figurePacketType(final Header header) {
