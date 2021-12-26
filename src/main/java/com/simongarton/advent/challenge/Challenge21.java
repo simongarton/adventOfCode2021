b5dc7b9a63597e484440f41e1cde2e2b1f8fcaac
Simon Garton <simon.garton@gmail.com>
2021-12-27 06:52:45 +1300

    Better idea, now taking too long.

--- src/main/java/com/simongarton/advent/challenge/Challenge21.java
+++ src/main/java/com/simongarton/advent/challenge/Challenge21.java
@@ -11,24 +11,26 @@ public class Challenge21 {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
 
-    private Map<Integer, Long> movesToWinPlayer1;
-    private Map<Integer, Long> movesToWinPlayer2;
-
     private static final String TITLE_1 = "Dirac Dice 1";
     private static final String TITLE_2 = "Dirac Dice 2";
 
-    /* I have a idea.
+    private static final int PART2_WIN = 21;
+    private static final int PART2_DEPTH = 24;
 
-    Take player 1 and run it recursively through all possibly games, finding out which move he finishes on
-    and building up a map of moves:times_found
+    private static final Map<Integer, Integer> moveUniverses = new HashMap<>();
 
-    Repeat for player 2.
-
-    Loop over all combinations and count up how many times 1 would have beaten 2.
-     */
+    static {
+        moveUniverses.put(3, 1);
+        moveUniverses.put(4, 3);
+        moveUniverses.put(5, 6);
+        moveUniverses.put(6, 7);
+        moveUniverses.put(7, 6);
+        moveUniverses.put(8, 3);
+        moveUniverses.put(9, 1);
+    }
 
     public void run(final String[] lines) {
-//        this.part1(lines);
+        this.part1(lines);
         this.part2(lines);
     }
 
@@ -40,17 +42,13 @@ public class Challenge21 {
         final long loser;
         while (true) {
             if (one.roll3Times(die)) {
-                System.out.println(one + " wins.");
                 loser = two.score;
                 break;
             }
-            System.out.println(one);
             if (two.roll3Times(die)) {
-                System.out.println(two + " wins.");
                 loser = one.score;
                 break;
             }
-            System.out.println(two);
         }
         final long result = (3L * (one.moves + two.moves)) * loser;
         this.logger.info(String.format("%s answer %d complete in %d ms",
@@ -69,10 +67,7 @@ public class Challenge21 {
         final long start = System.currentTimeMillis();
         final Player one = new Player(1, this.getPosition(lines[0]));
         final Player two = new Player(2, this.getPosition(lines[1]));
-//        final GameState gameState = new GameState(one.makeCopy(), two.makeCopy());
-//        gameState.kablooie();
-        this.buildOutcomeMaps(one, two);
-        final long result = 0;
+        final long result = this.buildOutcomeMaps(one, two);
         this.logger.info(String.format("%s answer %d complete in %d ms",
                 TITLE_2,
                 result,
@@ -80,17 +75,18 @@ public class Challenge21 {
         return result;
     }
 
-    private void buildOutcomeMaps(final Player one, final Player two) {
-        this.movesToWinPlayer1 = new HashMap<>();
-        this.buildOutcomeMap(one, this.movesToWinPlayer1);
+    private long buildOutcomeMaps(final Player one, final Player two) {
+        Map<Integer, Long> movesToWinPlayer1 = new HashMap<>();
+        System.out.println(LocalTime.now() + " starting");
+        this.buildOutcomeMap(one, movesToWinPlayer1);
         System.out.println(LocalTime.now() + " Player 1 done");
-        this.movesToWinPlayer2 = new HashMap<>();
-        this.buildOutcomeMap(two, this.movesToWinPlayer2);
+        Map<Integer, Long> movesToWinPlayer2 = new HashMap<>();
+        this.buildOutcomeMap(two, movesToWinPlayer2);
         System.out.println(LocalTime.now() + " Player 2 done");
         long oneWins = 0;
         long twoWins = 0;
-        for (final Map.Entry<Integer, Long> entry1 : this.movesToWinPlayer1.entrySet()) {
-            for (final Map.Entry<Integer, Long> entry2 : this.movesToWinPlayer1.entrySet()) {
+        for (final Map.Entry<Integer, Long> entry1 : movesToWinPlayer1.entrySet()) {
+            for (final Map.Entry<Integer, Long> entry2 : movesToWinPlayer1.entrySet()) {
                 if (entry1.getKey() <= entry2.getKey()) {
                     oneWins += entry1.getValue() + entry1.getValue();
                 } else {
@@ -98,52 +94,65 @@ public class Challenge21 {
                 }
             }
         }
-        System.out.println(LocalTime.now() + " Done one " + oneWins + " two " + twoWins);
+        System.out.println(LocalTime.now() + " one " + oneWins + " two " + twoWins);
+        return Math.max(oneWins, twoWins);
     }
 
-    private void buildOutcomeMap(final Player one, final Map<Integer, Long> movesToWinPlayer) {
-        final int stringLength = 24;
+    private void buildOutcomeMap(final Player player, final Map<Integer, Long> movesToWinPlayer) {
+        final int stringLength = PART2_DEPTH;
         movesToWinPlayer.clear();
 
-        String original = new String(new char[stringLength]).replace("\0", "1");
-        this.playWithSequence(one, original, movesToWinPlayer);
-        final String end = new String(new char[stringLength]).replace("\0", "3");
+        /*
+
+        I was treating the individual rolls, but they are not important, we only consider what we got to
+        on the third roll. So instead of having 3 rolls, each of which could be 1,2,3;  we have one roll,
+        which has possible outcomes of 3,4,5,6,7,8,9. However, the trick is that some are more likely than others ...
+        a total of 3 can only be generated in 1 universe, but 4 would be 3 universes. So I need a smaller sample size, but need
+        to change the scores when I get them back.
+
+         */
+
+        String sequence = new String(new char[stringLength]).replace("\0", "3");
+        this.playWithSequence(player, sequence, movesToWinPlayer);
+        final String end = new String(new char[stringLength]).replace("\0", "9");
         while (true) {
-            original = this.nudgeString(original, original.length() - 1);
-            if (original.equalsIgnoreCase(end)) {
-                this.playWithSequence(one, original, movesToWinPlayer);
+            sequence = this.nudgeString(sequence, sequence.length() - 1);
+            if (sequence.equalsIgnoreCase(end)) {
+                this.playWithSequence(player, sequence, movesToWinPlayer);
                 break;
             }
-            this.playWithSequence(one, original, movesToWinPlayer);
+            this.playWithSequence(player, sequence, movesToWinPlayer);
         }
     }
 
-    private void playWithSequence(final Player one, final String sequence, final Map<Integer, Long> movesToWinPlayer) {
-        final Player player = new Player(0, one.position);
-        final int moves = player.playWithDie(new SequenceDie(sequence));
-        movesToWinPlayer.put(moves, movesToWinPlayer.getOrDefault(moves, 0L) + 1);
+    private void playWithSequence(final Player player, final String sequence, final Map<Integer, Long> movesToWinPlayer) {
+        final Player newPlayer = new Player(0, player.position);
+        final int moves = newPlayer.playWithDie(new SequenceDie(sequence));
+        final long possibleGames = this.calculatePossibleGames(newPlayer.moveSequence);
+//        System.out.println(sequence + " moves " + moves + " games " + possibleGames);
+        movesToWinPlayer.put(moves, movesToWinPlayer.getOrDefault(moves, 0L) + possibleGames);
+    }
+
+    private long calculatePossibleGames(final String sequence) {
+        int possible = 1;
+        for (int i = 0; i < sequence.length(); i++) {
+            possible = possible * moveUniverses.get(Integer.parseInt(sequence.substring(i, i + 1)));
+        }
+        return possible;
     }
 
     private String nudgeString(final String original, int position) {
-//        System.out.println("nudging " +  original + " at " + position);
         int current = Integer.parseInt(original.substring(position, position + 1)) + 1;
         boolean rollOver = false;
-        if (current == 4) {
+        if (current == 10) {
             rollOver = true;
-            current = 1;
+            current = 3;
         }
         final String updated = original.substring(0, position) + current + original.substring(position + 1);
         if (!rollOver) {
-//            System.out.println("done nudging " +  updated + " at " + position);
             return updated;
         }
         position--;
-        if (position == -1) {
-            // this never executes ...
-//            System.out.println("all finished with " + "3" + updated.substring(1));
-            return "3" + updated.substring(1);
-        }
-//        System.out.println("going to nudge " + updated + " at " + position);
         return this.nudgeString(updated, position);
     }
 
@@ -171,14 +180,12 @@ public class Challenge21 {
             return (this.score >= 1000);
         }
 
-        private int moveTo(final int roll) {
-            this.moveSequence += roll + ",";
-            //System.out.println("player " + this.id + " got roll " + roll + " was at position " + this.position + " : " + this.moveSequence + " and score " + this.score);
+        private void moveTo(final int roll) {
+            this.moveSequence += roll + "";
             this.position = this.position + roll;
             while (this.position > 10) {
                 this.position -= 10;
             }
-            return this.position;
         }
 
         @Override
@@ -186,20 +193,9 @@ public class Challenge21 {
             return "Player " + this.id + " (moves " + this.moves + " score " + this.score + " position " + this.position + ")";
         }
 
-        public Player makeCopy() {
-            final Player player = new Player(this.id, this.position);
-            player.score = this.score;
-            player.moves = this.moves;
-            player.moveSequence = this.moveSequence;
-            return player;
-        }
-
         public int playWithDie(final SequenceDie sequenceDie) {
-            while (this.score <= 21) {
-                final int roll1 = sequenceDie.roll();
-                final int roll2 = sequenceDie.roll();
-                final int roll3 = sequenceDie.roll();
-                this.moveTo(roll1 + roll2 + roll3);
+            while (this.score < PART2_WIN) {
+                this.moveTo(sequenceDie.roll());
                 this.score += this.position;
                 this.moves += 1;
             }
@@ -250,67 +246,6 @@ public class Challenge21 {
             }
             System.out.println("->" + result);
             return result;
-        }
-    }
-
-    public static final class OutcomeGenerator {
-
-        private static long counter = 0;
-        private static long kablooies = 0;
-
-        private long id = 0;
-
-        private final Map<Integer, Long> movesToWinPlayer;
-
-        private final Player player;
-        private int roll;  // this is the next roll to make
-        private boolean complete;
-        private String moveSequence;
-
-        private OutcomeGenerator child1;
-        private OutcomeGenerator child2;
-        private OutcomeGenerator child3;
-
-        public OutcomeGenerator(final Player player, final Map<Integer, Long> movesToWinPlayer) {
-            this.id = ++counter;
-            this.roll = 1;
-            this.complete = false;
-            this.player = player;
-            this.movesToWinPlayer = movesToWinPlayer;
-        }
-
-        public void kablooie() {
-            System.out.println("kablooie for " + this.id + " on " + kablooies);
-            if (this.complete) {
-                return;
-            }
-            if (kablooies % 1000000 == 0) {
-//                System.out.println("kablooie for " + this.id + " on " + kablooies + " one wins " + oneWins);
-            }
-            kablooies++;
-            this.child1 = new OutcomeGenerator(this.player.makeCopy(), this.movesToWinPlayer);
-            this.child2 = new OutcomeGenerator(this.player.makeCopy(), this.movesToWinPlayer);
-            this.child3 = new OutcomeGenerator(this.player.makeCopy(), this.movesToWinPlayer);
-            this.child1.progressGame(this, 1);
-            this.child2.progressGame(this, 2);
-            this.child3.progressGame(this, 3);
-        }
-
-        private void progressGame(final OutcomeGenerator parent, final int diceRoll) {
-            this.roll = parent.roll + 1;
-            this.player.moveTo(diceRoll);
-            if (this.roll == 4) {
-                this.player.score += this.player.position;
-                this.player.moves += 1;
-                // any higher than 18 goes badly
-                if (this.player.score >= 21) {
-                    this.complete = true;
-                    this.movesToWinPlayer.put(this.player.moves, this.movesToWinPlayer.getOrDefault(this.player.moves, 0L) + 1);
-                    return;
-                }
-                this.roll = 1;
-            }
-            this.kablooie();
         }
     }
 }
