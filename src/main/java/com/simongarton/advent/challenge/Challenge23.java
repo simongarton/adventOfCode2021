905fed0c420451286555f15dca798d520a92d060
Simon Garton <simon.garton@gmail.com>
2022-01-09 16:18:52 +1300

    Remove duplicated dead code.

--- src/main/java/com/simongarton/advent/challenge/Challenge23.java
+++ src/main/java/com/simongarton/advent/challenge/Challenge23.java
@@ -3,11 +3,6 @@ package com.simongarton.advent.challenge;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Queue;
-
 public class Challenge23 {
 
     private static final String TITLE_1 = "Amphipod 1";
@@ -23,18 +18,10 @@ public class Challenge23 {
     protected long part1(final String[] lines) {
         final long start = System.currentTimeMillis();
         final long result = 0;
-        final ALU alu = new ALU(lines);
         this.logger.info(String.format("***  %s answer not found in %d ms",
                 TITLE_1,
                 System.currentTimeMillis() - start));
         return result;
-    }
-
-    private String padTo(final String s, final int size, final String replacement) {
-        if (s.length() > size) {
-            return new String(new char[size]).replace("\0", "-");
-        }
-        return new String(new char[size - s.length()]).replace("\0", replacement) + s;
     }
 
     protected long part2(final String[] lines) {
@@ -44,78 +31,5 @@ public class Challenge23 {
                 TITLE_2,
                 System.currentTimeMillis() - start));
         return result;
-    }
-
-    public static final class ALU {
-
-        Map<String, Integer> variables;
-
-        private final Queue<Integer> input;
-
-        private final String[] lines;
-
-        public ALU(final String[] lines) {
-            this.lines = lines;
-            this.variables = new LinkedHashMap<>();
-            this.variables.put("w", 0);
-            this.variables.put("x", 0);
-            this.variables.put("y", 0);
-            this.variables.put("z", 0);
-            this.input = new LinkedList<>();
-        }
-
-        public void run() {
-            for (final String line : this.lines) {
-                final String[] parts = line.split(" ");
-                final String instruction = parts[0];
-                final String operand1 = parts[1];
-                final String operand2 = parts.length > 2 ? parts[2] : null;
-//                System.out.println(instruction + ":" + operand1 + "," + operand2);
-                switch (instruction) {
-                    case "inp":
-                        this.variables.put(operand1, this.input.poll());
-                        break;
-                    case "add":
-                        this.variables.put(operand1, this.variables.get(operand1) + this.getNumberOrVariable(operand2));
-                        break;
-                    case "mul":
-                        this.variables.put(operand1, this.variables.get(operand1) * this.getNumberOrVariable(operand2));
-                        break;
-                    case "div":
-                        this.variables.put(operand1, Math.floorDiv(this.variables.get(operand1), this.getNumberOrVariable(operand2)));
-                        break;
-                    case "mod":
-                        this.variables.put(operand1, this.variables.get(operand1) % this.getNumberOrVariable(operand2));
-                        break;
-                    case "eql":
-                        this.variables.put(operand1, this.variables.get(operand1).equals(this.getNumberOrVariable(operand2)) ? 1 : 0);
-                        break;
-                }
-            }
-        }
-
-        private int getNumberOrVariable(final String operand) {
-            try {
-                return Integer.parseInt(operand);
-            } catch (final NumberFormatException ae) {
-                return this.variables.get(operand);
-            }
-        }
-
-        public void addInput(final int i) {
-            this.input.add(i);
-        }
-
-        public void dump() {
-            for (final Map.Entry<String, Integer> entry : this.variables.entrySet()) {
-                System.out.println(entry.getKey() + "=" + entry.getValue());
-            }
-        }
-
-        public void addMultipleInputs(final String s) {
-            for (int i = 0; i < s.length(); i++) {
-                this.addInput(Integer.parseInt(s.charAt(i) + ""));
-            }
-        }
     }
 }
